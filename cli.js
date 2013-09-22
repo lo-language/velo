280c8d5a401fce73dc0ada178064ff8bca76d720
Seth Purcell <spurcell@users.noreply.github.com>
2013-09-22 12:00:24 -0400

    Cleans up the cli a little bit

---
+++ cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var OpNode = require('./vm/OpNode');
+var Procedure = require('./vm/Procedure');
+var parser = require('./vm/GelParser').parser;
+var fs = require('fs');
+
+// node [path to this file] [path to input file]
+var source = fs.readFileSync(process.argv[2], 'utf8');
+
+parser.yy = {
+    OpNode: OpNode
+};
+
+var result = parser.parse(source);
+
+var proc = new Procedure([result]);
+
+proc.run();
